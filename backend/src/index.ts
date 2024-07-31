fa1621eac866dc909c54717738218e42b940b3c8
MeherajUlMahmmud <57069206+MeherajUlMahmmud@users.noreply.github.com>
2024-07-31 18:18:21 +0600

    updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,9 +6,8 @@ mongoose
 	.connect(config.dbUri)
 	.then(() => {
 		console.log("Connected to the database");
-		const PORT = process.env.PORT || 3000;
-		app.listen(PORT, () => {
-			console.log(`Server running on port ${PORT}`);
+		app.listen(config.port, () => {
+			console.log(`Server running on port ${config.port}`);
 		});
 	})
 	.catch((error) => {
